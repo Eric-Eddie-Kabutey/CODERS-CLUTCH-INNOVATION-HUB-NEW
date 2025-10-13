232e4e6206f38ec69da534a1c0b05fcb16c2d06b
touraye <touraye07@gmail.com>
2025-10-13 16:04:33 +0000

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["kedrus.io", "images.unsplash.com", "cdn.vectorstock.com"],
+  },
+};
+
+export default nextConfig;
