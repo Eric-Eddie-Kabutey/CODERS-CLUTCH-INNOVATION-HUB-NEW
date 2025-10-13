e5a09951f945bcb218bcdf9d240e60655f4947c4
touraye <touraye07@gmail.com>
2025-10-13 16:09:21 +0000

    add contact section

---
+++ app/contact/page.tsx
@@ -0,0 +1,18 @@
+
+import MapSection from "@/components/contact/MapSection";
+import ContactSection from "@/components/new-landing-page/ContactSection";
+import React from "react";
+
+const ContactUS = () => {
+  return (
+    <div>
+      {/* <MapWithControls /> */}
+      <div className="h-24 w-full"></div>
+      <MapSection />      
+
+      <ContactSection  /> 
+    </div>
+  );
+};
+
+export default ContactUS;
