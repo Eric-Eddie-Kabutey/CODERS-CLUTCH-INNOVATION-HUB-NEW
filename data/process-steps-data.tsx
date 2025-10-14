6129bb3f333bca506264c60bbbd18cfda8f29911
touraye <touraye07@gmail.com>
2025-10-14 14:31:44 +0000

    add data - step process

---
+++ data/process-steps-data.tsx
@@ -0,0 +1,17 @@
+export const PROCESS_STEPS_DATA = [
+  {
+    number: '01',
+    title: 'Ideation & Strategy',
+    description: "Through our innovation workshops, proof of concept, customer discovery, and product research we refine your idea and create a solid roadmap for development.",
+  },
+  {
+    number: '02',
+    title: 'Design & Development',
+    description: "By deeply understanding your vision, the needs of the market, and the behavioral patterns of your potential customers, we design and build your MVP quickly and efficiently.",
+  },
+  {
+    number: '03',
+    title: 'Lunch & Scale',
+    description: "We prepare your product for the market and help you attract users and investors.",
+  }, 
+];
