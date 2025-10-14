dd5ef51c4ba9fb7aa387a7a920f269a3ac9df6b5
touraye <touraye07@gmail.com>
2025-10-14 12:18:15 +0000

    add data - tabs

---
+++ data/tabs-data.ts
@@ -0,0 +1,34 @@
+export const TABS_DATA = [
+    {
+        id: 'programs',
+        title: 'Programs',
+        contentTitle: 'Apply to our programs',
+        contentDescription: 'Google for Startups offers a wide variety of programs to help startups grow and scale. Selected startups get bespoke Google support and guidance to help achieve their specific goals.',
+        buttonText: 'Explore programs',
+        imageSrc: "/home/program.png",
+    },
+    {
+        id: 'products',
+        title: 'Products',
+        contentTitle: 'Get Google technology',
+        contentDescription: 'Reach more customers, build better products, and run more efficiently by using innovative Google tools and products.',
+        buttonText: 'Find the right tools',
+        imageSrc: "/home/product.png",
+    },
+    {
+        id: 'people',
+        title: 'People',
+        contentTitle: 'Meet the right people',
+        contentDescription: 'Connect with like-minded founders, get expert guidance from Google mentors, network with industry leaders, and maybe even find your next investor at the Campus Cafe.',
+        buttonText: 'Learn about our alumni',
+        imageSrc: "/home/people.jpg",
+    },
+    {
+        id: 'spaces',
+        title: 'Spaces',
+        contentTitle: 'Discover Campus',
+        contentDescription: 'Google for Startups Campuses offer physical spaces around the world where tech-based startups enjoy the best of Google with free trainings, mentoring, and a community of problem solvers.',
+        buttonText: 'Learn more',
+        imageSrc: "/home/space.jpg",
+    },
+];
